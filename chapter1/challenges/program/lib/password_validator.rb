680f6add64469c49300a1629a152be2357f60447
Sam Hunt <115251504+SamuelHunt2002@users.noreply.github.com>
2022-10-26 14:53:27 +0100

    Update password_validator.rb

--- chapter1/challenges/program/lib/password_validator.rb
+++ chapter1/challenges/program/lib/password_validator.rb
@@ -3,14 +3,5 @@
 # - include at least one of these characters: !, @, $, %, &
 
 def valid?(password)
-  if (password.length < 8)
-    return false
-  else
-    if (password =~ /[!@$%&]+/)
-      return true
-    else
-      return false
-    end
-  end
 end
 # Test 
