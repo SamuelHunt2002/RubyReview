722d45d6454dfc8c9d5e442e7dded50c27d3b6bc
Sam Hunt <huntsamuel2002@gmail.com>
2022-10-31 14:36:09 +0000

    A whole bunch of stuff

--- chapter3/challenges/drills/lib/1_arrays_and_hashes.rb
+++ chapter3/challenges/drills/lib/1_arrays_and_hashes.rb
@@ -7,99 +7,150 @@
 # fourth_element
 # takes one array as an arg
 # returns the fourth element
-
+def fourth_element(array)
+  return array[3]
+end
 # average
 # takes an array of numbers
 # returns the average (the mean)
-
+def average(array)
+  return array.sum / array.count
+end
 # lowest_squared
 # takes an array of numbers
 # returns the lowest number squared
-
+def lowest_squared(array)
+  return array.sort[0] * array.sort[0]
+end
 # highest_squared
 # takes an array of numbers
 # returns the highest number squared
-
+def highest_squared(array)
+  return array.sort[-1] * array.sort[-1]
+end
 # Hashes I
 
 # values_summed
 # takes a hash where the values are numbers
 # returns the total of all the values
-
+def values_summed(hash)
+  return hash.values.sum
+end
 # first_two_keys
 # takes a hash
 # returns the first two keys
-
+def first_two_keys(hash)
+  return hash.keys[0..1]
+end
 # first_two_values
 # takes a hash
 # returns the first two values
-
+def first_two_values(hash)
+  return hash.values[0..1]
+end
 # Arrays II
 
 # starts_with_a
 # takes one array as an arg
 # returns only elements starting with 'a'
+def starts_with_a(array)
+  array.select { | elements | elements[0] == 'a'}
+end
 
 # starts_with_a_vowel
 # takes an array as an arg
 # returns only the elements that start with a vowel
-
+def starts_with_a_vowel(array)
+  array.select { |element| element[0] == 'a' || element[0] == 'e' || element[0] == 'i' || element[0] == 'o' || element[0] == 'u'}
+end
 # reverse_each_element
 # takes an array as an arg
 # reverses each element
 # E.g. when the arg is ['one', 'two']
 # it returns ['eno', 'owt']
-
+def reverse_each_element(array)
+  array.map { |element| element.reverse}
+end
 # sort_by_last_letter
 # takes one array as an arg
 # returns the array, sorted by the last letter
-
+def sort_by_last_letter(array)
+  array.sort_by {|element| element[-1]}
+end
 # greater_than_5
 # takes one array as an arg
 # returns only numbers greater than 5
-
+def greater_than_5(array)
+  array.select {|element| element > 5}
+end
 # greater_than
 # takes one array and one number as args
 # returns only the elements that are greater than the number provided
-
+def greater_than(array,number)
+  array.select {|element| element > number}
+end
 # less_than
 # takes one array and one number as args
 # returns only the elements that are less than the number provided
-
+def less_than(array, number)
+  array.select{|element| element < number}
+end
 # words_shorter_than
 # takes one array and one number as args
-# returns only the elements than have fewer characters than the number provided
-
+# returns only the elements that have fewer characters than the number provided
+def words_shorter_than(array, number)
+  array.select{|element| element.length < number}
+end
 # all_above?
 # takes an array and a number as args
 # returns true if all elements are greater than the number provided
-
+def all_above?(array,number)
+  newArray = array.select{|element| element > number}
+  newArray == array
+end
 # all_below?
 # takes an array and a number as args
 # returns true if all elements are less than the number provided
-
+def all_below?(array,number)
+  newArray = array.select{|element| element < number}
+  newArray == array
+end
 # multiply_each_by
 # takes one array and one number as args
 # returns a new array with each element multiplied by the number provided
-
+def multiply_each_by(array,number)
+array.map{|element| element * number}
+end
 # Hashes II
 
 # where_value_below
 # takes a hash and a number as args
 # returns key value pairs where the value is less than the number provided
-
+def where_value_below(hash,number)
+  hash.select{|key, value|value.to_i < number}
+end
 # where_key_starts_with
 # takes a hash and a letter as args
 # returns key value pairs where the key starts with the letter provided
-
+def where_key_starts_with(hash,letter)
+  hash.select{|key, value|key[0] == letter}
+end
 # add_key_value_pair
 # takes a hash, a key and a value as args
 # returns a the hash with the new key and value added
-
+def add_key_value_pair(hash,key,value)
+  hash[key] = value
+  return hash
+end
 # sort_by_key
 # takes a hash
 # sorts key-value pairs by key
-
+def sort_by_key(hash)
+  hash.sort.to_h
+end
 # sort_by_value
 # takes a hash
 # sorts key-value pairs by value
+def sort_by_value(hash)
+  hash.sort_by{|k, v| v}.to_h
+end
--- chapter3/challenges/drills/lib/2_classes.rb
+++ chapter3/challenges/drills/lib/2_classes.rb
@@ -32,15 +32,31 @@
 # > end
 
 # Greeter
+class Greeter
 # hello
+def hello(name)
 # Takes one string as an arg (a name)
 # Returns 'Hello, Rita', if the arg is 'Rita'
+  return "Hello, " + name
+end
+
 # goodbye
 # Takes one string as an arg (a name)
+def goodbye(name)
+    return "Goodbye, " + name
+end
+
 # Returns 'Goodbye, Sam', if the arg is 'Sam'
 # goodnight
+def goodnight(name)
+    return "Goodnight, " + name
+end
 # Takes one string as an arg (a name)
 # Returns 'Goodnight, Jo', if the arg is 'Jo'
+def goodmorning(name)
+    return "Goodmorning, " + name
+end
+end
 # goodmorning
 # Takes one string as an arg (a name)
 # Returns 'Goodmorning, Alex', if the arg is 'Alex'
@@ -51,46 +67,128 @@
 # adds it to the list of items
 # items
 # returns everything that has been added to the basket
-
+class Basket
+  def initialize
+    @shoppinglist = Array.new()
+  end
+def add(arg)
+@shoppinglist << arg
+end
+def items
+return @shoppinglist
+end
+end
 # Basket2
+class Basket2
+  def initialize
+    @shoppinglist = Array.new()
+  end
 # add
+def add(item,amount)
 # takes two arguments: a string for the item and a number for the amount of that item
 # adds both pieces of information to a list of items
+listofitems = {
+  "item" => item,
+  "amount" => amount
+}
+@shoppinglist << listofitems
+end
 # contents
+def contents
+  return @shoppinglist
+end
+end
 # returns everything that has been added to the basket
 # eg: after having added "carrots", 3
 # contents returns [{"item" => "carrots", "amount" => 3}]
 
 # Calculator
+class Calculator
+  def initialize
+    @operationhistory = Array.new()
+  end
 # add
+def add(numberOne,numberTwo)
+  operationrecorder = {
+    "operation" => "add",
+    "arguments" => [numberOne, numberTwo],
+    "result" => numberOne + numberTwo
+  }
+  @operationhistory << operationrecorder
+  return numberOne+numberTwo
+end
 # takes two numbers as args
 # returns the total
 # multiply
 # takes two numbers as args
 # multiplies one by the other
 # returns the result
+def multiply(numberOne,numberTwo)
+  operationrecorder = {
+    "operation" => "multiply",
+    "arguments" => [numberOne, numberTwo],
+    "result" => numberOne * numberTwo
+  }
+  @operationhistory << operationrecorder
+  return numberOne * numberTwo
+end
 # subtract
 # takes two numbers as args
 # subtracts the second from the first
 # returns the result
+def subtract(numberOne,numberTwo)
+  operationrecorder = {
+    "operation" => "subtract",
+    "arguments" => [numberOne, numberTwo],
+    "result" => numberOne - numberTwo
+  }
+  @operationhistory << operationrecorder
+  return numberOne - numberTwo
+end
 # divide
 # takes two numbers as args
 # divides the first by the second
 # returns the result
+def divide(numberOne,numberTwo)
+  operationrecorder = {
+    "operation" => "divide",
+    "arguments" => [numberOne, numberTwo],
+    "result" => numberOne / numberTwo
+  }
+  @operationhistory << operationrecorder
+  return numberOne / numberTwo
+end
 # history
 # takes no args
 # returns an array containing hashes of the operation, arguments and results of all previous calculations
-# eg: after multiplying 4 and 6, the history is 
+# eg: after multiplying 4 and 6, the history is
 # [{"operation" => "multiply", "arguments" => [4, 6], "result" => 24}]
-
+def history
+  return @operationhistory
+end
+end
 # Cohort
+class Cohort
+  def initialize
+    @studentslist = Array.new()
+  end
 # add_student
+def add_student(hash)
+  @studentslist << hash
+end
 # takes one hash, representing a student, as an arg.
 # E.g. {'name' => 'Jo', 'employer' => 'NASA'}
 # adds the new student to the list of students
 # students
+def students
+  return @studentslist
+end
 # returns all the students who have been added to the cohort
 # employed_by
+def employed_by(selectemployer)
+ @studentslist.select {|student| student['employer'] == selectemployer}
+end
+end
 # takes one string, the name of an employer, as an arg.
 # E.g. 'NASA'
 # returns only the students who work for that employer
@@ -109,22 +207,45 @@
     {'name' => 'home', 'building' => '10', 'street' => 'South Street'}
   ]
 }
-
+class Person
+  def initialize(hash)
+    @hash = hash
+  end
 # work_address
 # takes no args
 # returns the work address in a nice format
 # E.g. '50 Commercial Street'
+def work_address
+  return "#{@hash['addresses'][0]['building']} #{@hash['addresses'][0]['street']}"
+end
+def home_address
+  return "#{@hash['addresses'][1]['building']} #{@hash['addresses'][1]['street']}"
+end
+
 # home_address
 # takes no args
 # returns the home address in a nice format
 # E.g. '10 South Street'
+
 # pets
 # takes no args
 # returns a nice summary of the person's pets
-
 # E.g.
 # Alex has 3 pets
 # - a cat called Arthur
 # - a dog called Judith
 # - a goldfish called Gwen
 #
+def pets
+  name = @hash['name']
+  amountofpets = @hash['pets'].length
+  returnsentence = "#{name} has #{amountofpets} pets"
+  i = 0
+  while i <= amountofpets-1
+    returnsentence = returnsentence + "\n- "+ "a #{@hash['pets'][i]['animal']} called #{@hash['pets'][i]['name']}"
+    i = i + 1
+  end
+  return returnsentence + "\n"
+end
+
+end
